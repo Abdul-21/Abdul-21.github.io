fd9abb159ace75a8e8e0f0c31096bba30a7a527c
Abdul-21 <53537785+Abdul-21@users.noreply.github.com>
2021-04-02 11:10:09 -0500

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -17,7 +17,6 @@ targets.forEach(target => {
     })
     .from(target, {opacity: 0, duration: 0.2}, 0)
     .to(target, {opacity: 0, duration: 0.2}, 0.8)
-<<<<<<< HEAD
   });
 
   targets.forEach(target => {
@@ -27,48 +26,3 @@ targets.forEach(target => {
     .from(target,{x:0, opacity: 0, duration: .5})
     .to(target,{ left:"50%", top:"50%", opacity: 1, duration: .5, ease:Power2.easeIn})
   });
-
-  const intro = document.querySelector('.intro');
-const v = intro.querySelector('video');
-const t = intro.querySelector('h1');
-
-const section = document.querySelector('section');
-const end = section.querySelector('h1');
-
-let con = new ScrollMagic.Controller();
-let  scene = new ScrollMagic.Scene({
-    duration: 9000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-.setPin(intro)
-.addTo(con);
-
-const textAnim = TweenMax.fromTo(t,3,{opacity: 1}, {opacity: 0});
-
-let  s2 = new ScrollMagic.Scene({
-    duration: 500,
-    triggerElement: intro,
-    triggerHook: 0
-})
-.setTween(textAnim)
-.addTo(con);
-
-let accel = 0.5;
-let scrollPos = 0;
-let delay = 0;
-
-scene.on('update', e => {
-    scrollPos = e.scrollPos / 1000;
-
-    
-});
-
-setInterval(() => {
-    delay += (scrollPos - delay) * accel;
-    v.currentTime = delay;
-}, 70)
-
-=======
-  });
->>>>>>> parent of 47a6f8c (Adding animation)
